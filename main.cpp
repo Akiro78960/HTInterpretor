603493af5d6dcf443790c4167f325be21c4f2d22
Clovis Perin <cperin@digitaldistrict-fx.com>
2017-11-16 17:52:51 -0500

    Adds input/output part

--- main.cpp
+++ main.cpp
@@ -1,38 +1,43 @@
-// basic file operations
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <vector>
 using namespace std;
 
 int main ( int argc, char *argv[] ) {
 
+    fstream file;
+    std::vector<std::string> v;
+    int tabCount = 0;
+
     if(argc == 1){
-        cout << argc << " argument only";
-        fstream file;
+        cout << "no arguments, using input.doc"  << endl;
         file.open("input.cdoc", ios_base::in);
-
-        if (!file.is_open()){
-            cout << "create a input.cdoc file in the current directory!\n";
-            return 1;
-        }
-
-        file.close();
-
     }else if(argc == 2){
-        cout << argc << "    " << argv[1];
-        fstream file;
+        cout << "input file: " << argv[1] << endl;
         file.open(argv[1], ios_base::in);
+    }
+    if (!file.is_open()){
+      cout << "File does not exist yet !\n";
+      return 1;
+    }
 
-        if (!file.is_open()){
-            cout << "File does not exist yet !\n";
-            return 1;
-        }
+    string line;
+    while(getline(file, line)){
+         v.push_back(line);
+    }
+    file.close();
 
-        file.close();
+    for (int i = 0; i < v.size(); i++) {
+        cout << v[i] << endl;
     }
 
     ofstream myfile;
     myfile.open ("output.html");
-    myfile << "<p> Writing this to a file. </p> \n";
+
+    for (size_t i = 0; i < v.size(); i++) {
+        myfile << v[i] << endl;
+    }
     myfile.close();
 
     return 0;
